fac09b5391897481c12d4ffe6d0ac060b27c37cb
Ziheng Chen <zihengchen@Zihengs-MacBook-Pro.local>
2018-03-06 00:09:18 -0600

    update

--- scripts/pytorch_libraries/model_NeuralStyleTransfer.py
+++ scripts/pytorch_libraries/model_NeuralStyleTransfer.py
@@ -10,6 +10,7 @@ import torch
 import torchvision
 import torch.nn as nn
 import numpy as np
+from pylab import *
 
 
 use_cuda = torch.cuda.is_available()
@@ -19,39 +20,31 @@ dtype = torch.cuda.FloatTensor if use_cuda else torch.FloatTensor
 
 # Load image file and convert it into variable
 # unsqueeze for make the 4D tensor to perform conv arithmetic
-def load_image(image_path, transform=None, max_size=None, shape=None):
+def image_loader(image_path, transform):
     image = Image.open(image_path)
-    
-    if max_size is not None:
-        scale = max_size / max(image.size)
-        size = np.array(image.size) * scale
-        image = image.resize(size.astype(int), Image.ANTIALIAS)
-    
-    if shape is not None:
-        image = image.resize(shape, Image.LANCZOS)
-    
-    if transform is not None:
-        image = transform(image).unsqueeze(0)
+    image = transform(image).unsqueeze(0)
     
     return image.type(dtype)
+
+def tensor_shower(tensor,imH,imW, title = None):
+    unloader = transforms.ToPILImage()
+    image    = tensor.clone().cpu()  # we clone the tensor to not do changes on it
+    image    = image.view(3, imH, imW)  # remove the fake batch dimension
+    image    = unloader(image)
+    plt.imshow(image)
+    plt.axis('off')
+    if not title is None:
+        plt.title(title)
+        
 
 # Pretrained VGGNet 
 class VGGNet(nn.Module):
     def __init__(self):
-        """Select conv1_1 ~ conv5_1 activation maps."""
         super(VGGNet, self).__init__()
-        self.select = ['0','5', '10', '19', '28'] 
-        self.vgg = models.vgg19(pretrained=True).features
+        self.select = ['0','2', '5', '7', '10'] 
+        self.vgg    = models.vgg19(pretrained=True).features
         
     def forward(self, x):
-        """Extract 5 conv activation maps from an input image.
-        
-        Args:
-            x: 4D tensor of shape (1, 3, height, width).
-        
-        Returns:
-            features: a list containing 5 conv activation maps.
-        """
         features = []
         for name, layer in self.vgg._modules.items():
             if int(name)<=int(self.select[-1]):
@@ -59,6 +52,8 @@ class VGGNet(nn.Module):
                 if name in self.select:
                     features.append(x)
         return features
+
+
 
 
 
